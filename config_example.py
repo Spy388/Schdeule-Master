d6ac29671a6fd7f9f11cf4967bd176635b0806be
Spy388 <44740760+Spy388@users.noreply.github.com>
2020-07-13 17:03:44 -0600

    update

---
+++ config_example.py
@@ -0,0 +1,14 @@
+postgres = {
+    "host": "host", # 5432 port is default, don't include it
+    "database": "database",
+    "user": "user",
+    "password": "password"
+    }
+
+bot = {
+    "version" : "dev", # dev/prod - determines which bot token is used + commands availability
+    "dev_token": "login_token",
+    "prod_token": "login_token"
+    }
+
+teamup_apikey = "" # team up api key from https://teamup.com/api-keys/request
